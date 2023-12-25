e856dc6287da700b0a7e23adbe70d8b778d04b5f
4eshir <g.kalashnik@mail.ru>
2023-12-25 11:59:35 +0400

    25-12-2023 add fix

--- app/Http/Controllers/Api/EntryApiController.php
+++ app/Http/Controllers/Api/EntryApiController.php
@@ -35,7 +35,7 @@ class EntryApiController extends Controller
         $uIds = [];
         foreach ($users as $user) $uIds[] = $user->id;
 
-        $entries = OlympiadEntryWorkApi::whereIn('children_event_id', $ceIds)->whereIn('user_id', $uIds)->where('status', '!=', 1)->get();
+        $entries = OlympiadEntryWorkApi::whereIn('children_event_id', $ceIds)->whereIn('user_id', $uIds)->whereNull('status')->get();
 
         // КАК СЛОЖНО ДЕЛАТЬ БЛЯДСКИЕ ДЖОЙНЫ В ELOQUENT!!!
 
